d15ea1138287a2c37d6890cab673b0d0cca315f9
rakkogurumi <rakkononuigurumi@gmail.com>
2016-06-30 23:36:53 +0900

    040

---
+++ 040/040d.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void){
+	int N,M,Q;
+	scanf("%d %d",&N,&M);
+	int a[M+1],b[M+1],y[M+1];
+	int i;
+	for(i=1;i<M+1;i++){
+		scanf("%d %d %d",&a[i],&b[i],&y[i]);
+	}
+	scanf("%d",&Q);
+	int v[Q+1],w[Q+1];
+	for(i=1;i<Q+1;i++){
+		scanf("%d %d %d",&v[i],&w[i]);
+	}
+
+	for(i=0;i<Q;i++){
+		
+	}
+}
